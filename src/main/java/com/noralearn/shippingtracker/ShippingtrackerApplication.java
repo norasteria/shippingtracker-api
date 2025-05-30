ecde8d5baed15fb86ade2e7647bfd90632ad2b25
noraasteria <nora_asteria@yahoo.cp.od>
2025-05-30 22:36:07 +0700

    Init the project

---
+++ src/main/java/com/noralearn/shippingtracker/ShippingtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.noralearn.shippingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noralearn/shippingtracker/ShippingtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noralearn.shippingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
