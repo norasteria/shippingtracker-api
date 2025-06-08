40a373d6def1d52b08905cb2ab620e09a3b62991
noraasteria <nora_asteria@yahoo.cp.od>
2025-06-08 15:20:41 +0700

    fix sql script for company_name should be not null and create unique constraint of customer slug

--- src/main/resources/db/migration/V3__add_columns_customer_table.sql
+++ src/main/resources/db/migration/V3__add_columns_customer_table.sql
@@ -1,5 +1,7 @@
-ALTER TABLE customers RENAME COLUMN name to company_name;
+ALTER TABLE customers RENAME COLUMN name to company_name NOT NULL;
 ALTER TABLE customers ADD COLUMN city TEXT;
 ALTER TABLE customers ADD COLUMN address TEXT;
 ALTER TABLE customers ADD COLUMN province TEXT;
 ALTER TABLE customers ADD COLUMN slug TEXT;
+
+CREATE UNIQUE INDEX customer_slug ON customers(slug);
