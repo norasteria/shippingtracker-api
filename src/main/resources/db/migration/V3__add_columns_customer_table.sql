7f26c46fb03a0b4d10cb068aa86350b20fbcc008
noraasteria <nora_asteria@yahoo.cp.od>
2025-06-08 20:31:15 +0700

    fix set company_name not null

--- src/main/resources/db/migration/V3__add_columns_customer_table.sql
+++ src/main/resources/db/migration/V3__add_columns_customer_table.sql
@@ -1,4 +1,5 @@
-ALTER TABLE customers RENAME COLUMN name to company_name NOT NULL;
+ALTER TABLE customers RENAME COLUMN name to company_name;
+ALTER TABLE customers ALTER COLUMN company_name SET NOT NULL;
 ALTER TABLE customers ADD COLUMN city TEXT;
 ALTER TABLE customers ADD COLUMN address TEXT;
 ALTER TABLE customers ADD COLUMN province TEXT;
